1ae4234d09cb609b1afda9e4fa1ba9534d75bc5b
Aldair <aldairtorres507@gmail.com>
2025-05-10 21:51:02 -0500

    hay errroes

--- src/app.ts
+++ src/app.ts
@@ -6,19 +6,21 @@ import cookieParser from "cookie-parser";
 import { authRouter } from "./routes/auth.routes";
 import morgan from "morgan";
 import { adminRoutes } from "./routes/admin.routes";
+import cors from "cors"
 
 const app = express();
 
 connectDB();
 
 app.use(morgan("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser()); // <-- antes de las rutas
 
-app.use("/api", usuarioRouter);
+app.use("/api/", usuarioRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/admin", adminRoutes);
-app.get("/test", (req, res) => {
+app.get("/test/", (req, res) => {
   res.json({ success: true, message: "Ruta de prueba funcionando" });
 });
 
